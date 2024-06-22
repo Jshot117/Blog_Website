b2b2a45289ccac7e4136c96ad296f673973ca867
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-06-22 03:31:46 -0400

    updated BlogCard.tsx

--- Blog/src/components/BlogCard.tsx
+++ Blog/src/components/BlogCard.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 interface BlogCardProps {
     title: string;
-    link: { name : string , url : string };
+    link: { name: string, url: string };
     description: string;
 }
 
-const BlogCard: React.FC<BlogCardProps> = ({title, link, description }) => {
+const BlogCard: React.FC<BlogCardProps> = ({ title, link, description }) => {
     return (
-        <div className = "">
-            <div className = "flex-col text-center">
-                <h2>{title}</h2>
+        <div className="m-10 border-4 ">
+            <div className="flex-col text-center mb-5 mt-5">
+                <h2 className="ml-1 text-left font-bold">{title}</h2>
                 <p>{description}</p>
-                <a href = {link.url}>{link.name}</a>
-            </div>  
+                <div className="text-blue-900">
+                    <a href={link.url}>{link.name}</a>
+                </div>
+            </div>
         </div>
     )
 }
