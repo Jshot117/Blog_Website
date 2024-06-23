d424ba2a01518e019c130523fbdaf38c76de8b57
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-06-22 23:46:16 -0400

    updated BlogCard

--- Blog/src/components/BlogCard.tsx
+++ Blog/src/components/BlogCard.tsx
@@ -8,7 +8,7 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ title, link, description }) => {
     return (
-        <div className="m-10 border-4 ">
+        <div className="mr-48 ml-48 mb-10 mt-10 border-4 ">
             <div className="flex-col text-center mb-5 mt-5">
                 <h2 className="ml-1 text-left font-bold">{title}</h2>
                 <p>{description}</p>
