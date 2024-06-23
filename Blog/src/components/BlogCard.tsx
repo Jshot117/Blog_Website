677cf50e32355e1a8dc98046a70801397103de3b
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-06-23 01:28:56 -0400

    updated borders on BlogCard

--- Blog/src/components/BlogCard.tsx
+++ Blog/src/components/BlogCard.tsx
@@ -8,9 +8,9 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ title, link, description }) => {
     return (
-        <div className="mr-48 ml-48 mb-10 mt-10 border-4 ">
+        <div className="mr-5 ml-5 lg:mr-96 lg:ml-96 md:mr-48 md:ml-48 mb-10 mt-10 border-4 border-black bg-gray-50">
             <div className="flex-col text-center mb-5 mt-5">
-                <h2 className="ml-1 text-left font-bold">{title}</h2>
+                <h2 className="text-center md:ml-1 md:text-left font-bold">{title}</h2>
                 <p>{description}</p>
                 <div className="text-blue-900">
                     <a href={link.url}>{link.name}</a>
