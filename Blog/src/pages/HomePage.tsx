61042c43adf1fa52808c043fee70fc68275a8412
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-06-22 23:46:04 -0400

    updated HomePage

--- Blog/src/pages/HomePage.tsx
+++ Blog/src/pages/HomePage.tsx
@@ -3,28 +3,28 @@ import BlogCard from '../components/BlogCard'
 import BlogHeader from '../components/BlogHeader'
 
 const HomePage: React.FC = () => {
-    const blogs = [
-        {
-            title: "My First Blog Post",
-            link: { name: "Read More", url: "/home/FirstBlog" },
-            description: "This is the content of my first blog post. I am excited to share my thoughts with you."
+    const blogs = [{
+            title: "My Third Blog Post",
+            link: { name: "Read More", url: "/home/ThirdBlog" },
+            description: "This is the content of my third blog post. I am excited to share my thoughts with you."
         },
+        
         {
             title: "My Second Blog Post",
             link: { name: "Read More", url: "/home/SecondBlog" },
             description: "This is the content of my second blog post. I am excited to share my thoughts with you."
         },
         {
-            title: "My Third Blog Post",
-            link: { name: "Read More", url: "/home/ThirdBlog" },
-            description: "This is the content of my third blog post. I am excited to share my thoughts with you."
-        }
+            title: "My First Blog Post",
+            link: { name: "Read More", url: "/home/FirstBlog" },
+            description: "My first blog post I want to be my an introduction to myself and my expereince creating my blog. "
+        },
         
     ]
 
     return (
         <>
-            <BlogHeader title={'Jonnathans Blog'} links={[{ "name": "Instagram", "url": "https://www.instagram.com/jonn_ig/" },
+            <BlogHeader title={"Jonnathan's Blog"} links={[{ "name": "Instagram", "url": "https://www.instagram.com/jonn_ig/" },
             { "name": "linkedIn", "url": "https://www.linkedin.com/in/jonnathan-saavedra/" },
             { "name": "Personal Portfolio", "url": "https://jshot117.github.io/Portfolio_Website/" }]} />
             <div className="grid grid-cols-1">
