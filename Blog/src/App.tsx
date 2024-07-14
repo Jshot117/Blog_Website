7d0b72da9b7730660d8bb508701a23c3eb5ccdfd
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-07-14 01:39:17 -0400

    fixed double routes in app.tsx

--- Blog/src/App.tsx
+++ Blog/src/App.tsx
@@ -1,24 +1,19 @@
-import './App.css'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import { Route } from 'react-router-dom'
-import BlogPost from './pages/BlogPosts'
-
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BlogPost from './pages/BlogPosts';
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-        </Routes>
-        <Routes>
           <Route path="/MyFirstBlog" element={<BlogPost postId="MyFirstBlog" />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
 export default App;
