dbd86761909a59ddcd72392b2f8a9c2cfe5ecb4c
Jonnathan Saavedra <jonnjstart@gmail.com>
2024-07-18 00:20:51 -0400

    make title of blog cards clickable

--- Blog/src/components/BlogCard.tsx
+++ Blog/src/components/BlogCard.tsx
@@ -11,7 +11,7 @@ const BlogCard: React.FC<BlogCardProps> = ({ title, link, description, date }) =
     return (
         <div className="bg-gray-200 m-10 border-4 rounded-lg shadow-lg p-8  max-w-4xl mx-auto min-h-72 flex flex-col justify-between" style={{ width: "95vw" }}>
             <div className="flex-grow">
-                <h2 className="font-bold text-2xl mb-2">{title}</h2>
+                <h2 className="font-bold text-2xl mb-2"><a href={link.url} className="hover:underline text-lg">{title}</a></h2>
                 <h3 className="text-lg mb-4">{date}</h3>
                 <p className="text-gray-700">{description}</p>
             </div>
