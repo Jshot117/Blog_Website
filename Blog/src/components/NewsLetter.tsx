ef59dd1055e78eb46f358304958989a859e6afac
Jshot117 <jonnjstart@gmail.com>
2025-02-13 12:58:51 -0500

    added really bad error handling

--- Blog/src/components/NewsLetter.tsx
+++ Blog/src/components/NewsLetter.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const NewsLetter = () => {
     const [email, setEmail] = useState("");
     const [error, setError] = useState("");
+    const [success, setSuccess] = useState("")
 
     const validEmail = (email: String) => {
         const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -26,21 +27,30 @@ const NewsLetter = () => {
             if (!response.ok) {
                 if (response.status == 409) {
                     setError("Already entered email")
+                    setSuccess("");
                 }
                 else {
                     throw new Error("An error occurred. Please try again later");
                     console.log(email);
                     setEmail("");
-                    setError("");
+                    setSuccess("");
+                    setError("Could'nt add Email");
                 }
+            }
+            else {
+                setEmail("");
+                setError("");
+                setSuccess("Successfully Subscribed")
             }
         }
         catch (error) {
             if (error === 409) {
                 setError("Already entered email")
+                setSuccess("");
             }
             else {
                 setError("An error occurred. Please try again later");
+                setSuccess("");
             }
             console.log(error);
             return;
@@ -58,6 +68,7 @@ const NewsLetter = () => {
         <div className="flex justify-center pb-10">
             <form onSubmit={handleSubmit} className="  mx-auto md:size-fit">
                 <div>{error && <p className="translate-y-8 translate-x-3 absolute text-red-500 mt-2">{error}</p>}</div>
+                <div>{success && <p className="translate-y-8 translate-x-3 absolute text-green-500 mt-2">{success}</p>}</div>
                 <div className="flex flex-row">
                     <div>
                         <input type="text" placeholder="Enter your email" value={email} onChange={handleChange} className="border rounded-l-md p-2" />
